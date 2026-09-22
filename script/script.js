e7fa65590f3b7d9f5091beed56a0a1743c6009b0
Carlos Henrique Sitóe <charlessitoe4@gmail.com>
2026-09-22 02:22:07 +0200

    Agent host session 3b31c9f5-1e65-4ec5-8689-b7ca69eb8737 - turn 1

--- script/script.js
+++ script/script.js
@@ -1,28 +1,24 @@
-//ativando o meu mobile
+$(function () {
+    const menu = $('nav#nav-esquerda ul.menu-principal');
+    const btnMenu = $('#menu-toggle');
+    const btnClose = $('#menu-close');
 
-function mostrarMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display','flex')
-    $('nav#nav-esquerda ul.menu-principal').addClass('animate__animated animate__fadeInRight animate__slow');
-
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display','none');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display','flex');
-}
-
-function esconderMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display','none');
-
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display','flex');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display','none');
-}
-
-let controle=true;
-
-$('nav#nav-esquerda ul#icone-menu li#menu').click(function(){
-    if(controle == true){
-        mostrarMenu();
-        controle = false;
-    }else{
-        esconderMenu();
-        controle = true;
+    function mostrarMenu() {
+        menu.css('display', 'flex')
+            .removeClass('animate__fadeOutRight')
+            .addClass('animate__animated animate__fadeInRight animate__slow');
+        btnMenu.css('display', 'none');
+        btnClose.css('display', 'flex');
     }
-});    
+
+    function esconderMenu() {
+        menu.css('display', 'none')
+            .removeClass('animate__animated animate__fadeInRight animate__fadeOutRight');
+        btnMenu.css('display', 'flex');
+        btnClose.css('display', 'none');
+    }
+
+    btnMenu.on('click', mostrarMenu);
+    btnClose.on('click', esconderMenu);
+    menu.find('a').on('click', esconderMenu);
+});
